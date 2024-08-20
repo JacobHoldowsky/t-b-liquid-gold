168ae35679eaed0adf65c1036e9c60637705bbaf
Yaakov Holdowsky <yholdowsky@aeiginsurance.com>
2024-08-20 07:21:54 -0400

    fixed hamburger menu to close when switching to a smaller screen.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link"; 
 import "./Header.css";
@@ -10,6 +10,23 @@ function Header({ cartItemCount }) {
     setIsOpen(!isOpen);
   };
 
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 768 && isOpen) {
+        closeMenu();
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [isOpen]);
+
   const scrollWithOffset = (el) => {
     const yCoordinate = el.getBoundingClientRect().top + window.pageYOffset;
     const yOffset = -100;
@@ -19,7 +36,7 @@ function Header({ cartItemCount }) {
   return (
     <header className="header">
       <div className="logo">
-        <Link to="/">
+        <Link to="/" onClick={closeMenu}>
           <img src="t&bLiquidGoldLogo.png" alt="TnB Liquid Gold" />
         </Link>
       </div>
@@ -31,36 +48,36 @@ function Header({ cartItemCount }) {
       <nav>
         <ul className={isOpen ? "active" : ""}>
           <li>
-            <Link to="/" onClick={toggleMenu}>Home</Link>
+            <Link to="/" onClick={closeMenu}>Home</Link>
           </li>
           <li>
-            <HashLink smooth to="/catalog#ourCollection" scroll={scrollWithOffset} onClick={toggleMenu}>
+            <HashLink smooth to="/catalog#ourCollection" scroll={scrollWithOffset} onClick={closeMenu}>
               Our Collection
             </HashLink>
           </li>
           <li>
-            <HashLink smooth to="/catalog#giftPackages" scroll={scrollWithOffset} onClick={toggleMenu}>
+            <HashLink smooth to="/catalog#giftPackages" scroll={scrollWithOffset} onClick={closeMenu}>
               Gift Packages
             </HashLink>
           </li>
           <li>
-            <HashLink smooth to="/catalog#corporateGifts" scroll={scrollWithOffset} onClick={toggleMenu}>
+            <HashLink smooth to="/catalog#corporateGifts" scroll={scrollWithOffset} onClick={closeMenu}>
               Corporate Gifts
             </HashLink>
           </li>
           <li>
-            <HashLink smooth to="/catalog#wholesale" scroll={scrollWithOffset} onClick={toggleMenu}>
+            <HashLink smooth to="/catalog#wholesale" scroll={scrollWithOffset} onClick={closeMenu}>
               Wholesale
             </HashLink>
           </li>
           <li>
-            <Link to="/about" onClick={toggleMenu}>About</Link>
+            <Link to="/about" onClick={closeMenu}>About</Link>
           </li>
           <li>
-            <Link to="/contact" onClick={toggleMenu}>Contact</Link>
+            <Link to="/contact" onClick={closeMenu}>Contact</Link>
           </li>
           <li className="cart-link">
-            <Link to="/checkout" onClick={toggleMenu}>
+            <Link to="/checkout" onClick={closeMenu}>
               Checkout
               {cartItemCount > 0 && (
                 <span className="cart-count">{cartItemCount}</span>
