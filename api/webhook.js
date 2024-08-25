3059760e873f25024142f0fa932124968fa459a9
Yaakov Holdowsky <yholdowsky@aeiginsurance.com>
2024-08-25 15:25:45 -0400

    e

--- api/webhook.js
+++ api/webhook.js
@@ -8,7 +8,7 @@ const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_SERVER,
   port: process.env.MAIL_PORT,
-  secure: process.env.MAIL_USE_TLS === "true",
+  secure: process.env.MAIL_USE_TLS === 'true',
   auth: {
     user: process.env.MAIL_USERNAME,
     pass: process.env.MAIL_PASSWORD,
