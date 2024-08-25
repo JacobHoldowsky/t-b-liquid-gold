41f9f1d2ec8940e5968e6bca1c0e49380f49abf4
Yaakov Holdowsky <yholdowsky@aeiginsurance.com>
2024-08-25 15:27:06 -0400

    added dotenv to webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,6 +1,9 @@
 const Stripe = require("stripe");
 const nodemailer = require("nodemailer");
 const { buffer } = require("micro");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
@@ -8,7 +11,7 @@ const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_SERVER,
   port: process.env.MAIL_PORT,
-  secure: process.env.MAIL_USE_TLS === 'true',
+  secure: process.env.MAIL_USE_TLS === "true",
   auth: {
     user: process.env.MAIL_USERNAME,
     pass: process.env.MAIL_PASSWORD,
