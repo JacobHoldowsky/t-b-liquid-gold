dce271276fd3cc8b390b7ebfeb4c6b6837bece11
Yaakov Holdowsky <yholdowsky@aeiginsurance.com>
2024-09-02 13:27:22 -0400

    added better footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,11 @@ function Footer() {
     <footer className="footer">
       <div className="footer-content">
         <p>&copy; 2024 T&B Liquid Gold. All rights reserved.</p>
-        <p className="holdowsky">Website developed by Y. Holdowsky. </p>
+        <p className="holdowsky">Website crafted by Y. Holdowsky</p>
+        <p className="holdowsky">Need a your own website?</p>
         <p className="holdowsky">
-          <a href="mailto:jacobedward1995@gmail.com?subject=Saw%20you%20on%20TandBeeLiquidGold.com!">
-            Let's Connect
+          <a href="mailto:jacobedward1995@gmail.com?subject=Saw%20your%20website%20on%20TandBeeLiquidGold.com!">
+            Reach out today!{" "}
           </a>
         </p>
       </div>
