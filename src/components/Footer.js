ab0be748454f6be3de37d1c11ac162cffe665a27
Yaakov Holdowsky <yholdowsky@aeiginsurance.com>
2024-09-02 13:29:18 -0400

    added period in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
     <footer className="footer">
       <div className="footer-content">
         <p>&copy; 2024 T&B Liquid Gold. All rights reserved.</p>
-        <p className="holdowsky">Website crafted by Y. Holdowsky</p>
+        <p className="holdowsky">Website crafted by Y. Holdowsky.</p>
         <p className="holdowsky">Need a your own website?</p>
         <p className="holdowsky">
           <a href="mailto:jacobedward1995@gmail.com?subject=Saw%20your%20website%20on%20TandBeeLiquidGold.com!">
