aeee64a614f289ac2c5c51081331daa92283d676
Yaakov Holdowsky <yholdowsky@aeiginsurance.com>
2024-09-04 09:11:34 -0400

    uxility footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,17 @@ function Footer() {
     <footer className="footer">
       <div className="footer-content">
         <p>&copy; 2024 T&B Liquid Gold. All rights reserved.</p>
-        <p className="holdowsky">Website developed by Y. Holdowsky.</p>
+        <p className="holdowsky">
+          Website developed by{" "}
+          <a
+            href="https://uxilitypro.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            UXilityPRO
+          </a>
+          .
+        </p>
         <p className="holdowsky">Need your own website?</p>
         <p className="holdowsky">
           <a href="mailto:jacobedward1995@gmail.com?subject=Reaching%20out%20from%20TandBeeLiquidGold.com!">
