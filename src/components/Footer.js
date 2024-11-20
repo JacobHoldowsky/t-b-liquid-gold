c18da0dee4bf4539e92fce938235af90d2f6b17d
Yaakov Holdowsky <yholdowsky@aeiginsurance.com>
2024-11-20 11:58:58 -0500

    added uxilitypro logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,24 +7,17 @@ function Footer() {
       <div className="footer-content">
         <p>&copy; 2024 T&B Liquid Gold. All rights reserved.</p>
         <p className="holdowsky">
-          Website developed by{" "}
-          <a
-            href="https://uxilitypro.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            UXilityPRO
-          </a>
-          .
-        </p>
-        <p className="holdowsky">Need your own website?</p>
-        <p className="holdowsky">
+          Website created by{" "}
           <a
             href="https://uxilitypro.com/contact"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Reach out today!{" "}
+            <img
+              className="logo-img"
+              src="UXilityPROLogo2.png"
+              alt="UXilityPRO Logo"
+            />
           </a>
         </p>
       </div>
