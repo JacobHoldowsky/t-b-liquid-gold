050099f46a837bc877911619ee27d8e9b110c90b
Jeremy Jacob <jeremyjacob101@gmail.com>
2026-08-06 12:28:58 +0300

    [found]

--- api/send-email.js
+++ api/send-email.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
       await resend.emails.send({
         from: 'contact@uxilitypro.com', // Update this with your verified domain
         to: process.env.PERSONAL_EMAIL,
-        subject: "New Contact Form Submission 1",
+        subject: "New Contact Form Submission",
         html: `
           <div style="font-family: Arial, sans-serif; color: #333; max-width: 600px; margin: 0 auto; padding: 20px; border: 1px solid #ddd; border-radius: 10px;">
             <h2 style="color: #7c2234; border-bottom: 2px solid #ddd; padding-bottom: 10px;">New Contact Form Submission</h2>
@@ -29,7 +29,7 @@ export default async function handler(req, res) {
       await resend.emails.send({
         from: 'contact@uxilitypro.com', // Update this with your verified domain
         to: email,
-        subject: "Thank you for contacting us 1",
+        subject: "Thank you for contacting us",
         html: `
           <div style="font-family: Arial, sans-serif; color: #333; max-width: 600px; margin: 0 auto; padding: 20px; border: 1px solid #ddd; border-radius: 10px;">
             <h2 style="color: #7c2234; border-bottom: 2px solid #ddd; padding-bottom: 10px;">Thank You for Reaching Out</h2>
